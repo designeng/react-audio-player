0f8febd76b1071fc03cdecbd91c5c2c756aeeafc
Denis Savenok <denissavenok@gmail.com>
2023-01-28 12:35:21 +0300

    Experiment with current time (not succeed)

--- example/js/main.jsx
+++ example/js/main.jsx
@@ -1,10 +1,15 @@
-import React, { createRef, useCallback } from 'react';
+import React, { createRef, useCallback, useState } from 'react';
 import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import ReactAudioPlayer from '../../dist/bundle';
+import '../css/style.css';
 
 const App = () => {
   const player = createRef(null);
+
+  const [time, setTime] = useState(0);
+
+  const currentPlayerTag = player.current?.audioEl?.current;
 
   useEffect(() => {
     player.current?.audioEl?.current.play();
@@ -18,17 +23,35 @@ const App = () => {
     player.current.audioEl.current.pause();
   }, []);
 
+  const playFrom = (time) => () => {
+    console.log(
+      'PLAY FROM, play...',
+      time,
+      player.current?.audioEl?.current.play
+    );
+    setTime(time);
+    player.current?.audioEl?.current.play();
+  };
+
   return (
     <>
       <ReactAudioPlayer
         src="/files/1.mp3"
         controls
-        currentTime={100}
+        currentTime={time}
         autoPlay={true}
         ref={player}
       />
       <button onClick={onPlay}>Play</button>
       <button onClick={onStop}>Stop</button>
+      <ul>
+        <li className="pointer">
+          <button onClick={playFrom(100)}>Play from 100 ms</button>
+        </li>
+        <li className="pointer">
+          <button onClick={playFrom(200)}>Play from 200 ms</button>
+        </li>
+      </ul>
     </>
   );
 };
